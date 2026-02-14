c424ec8c8e9890443e986af167ec90918bd9cfbd
yksb-x <127526695+yksb-x@users.noreply.github.com>
2026-02-14 12:55:53 +0900

    Create keys_ja.h

---
+++ config/keys_ja.h
@@ -0,0 +1,13 @@
+/* 日本語JIS配列用の定義（これだけあれば主要な記号はカバーできます） */
+#define JP_ZKHK GRAVE   // 半角/全角
+#define JP_CARET EQUAL  // ^
+#define JP_YEN   INT3   // ¥
+#define JP_AT    LBKT   // @
+#define JP_LBKT  RBKT   // [
+#define JP_RBKT  NONUS_HASH // ]
+#define JP_COLON SQT    // :
+#define JP_SQT   LS(N7) // '
+|#define JP_LPAR  LS(N8) // (
+#define JP_RPAR  LS(N9) // )
+#define JP_MINUS MINUS  // -
+#define JP_EQUAL LS(MINUS) // =
